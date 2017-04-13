26406d9171de6ffe59480066dac5f24a9d9a1bd7
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-04-13 18:04:44 +0100

    inital commit

---
+++ Assets/Scripts/imageSwapnScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class imageSwapnScript : MonoBehaviour
+{
+    //add more then do in order
+    public GameObject image1;
+    public GameObject image2;
+    public GameObject image3;
+    public GameObject image4;
+    public GameObject image5;
+    public GameObject image6;
+    public GameObject image7;
+    public GameObject image8;
+    public GameObject image9;
+    public GameObject image10;
+    public string curImage;
+    public int imageCount=1;
+
+    private GameObject curObj;
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            spawnImage();
+        }
+    }
+
+    private void spawnImage()
+    {
+        switch (imageCount)
+        {
+            case 1:
+                curObj = image1;
+                break;
+            case 2:
+                curObj = image2;
+                break;
+            case 3:
+                curObj = image3;
+                break;
+            case 4:
+                curObj = image4;
+                break;
+            case 5:
+                curObj = image5;
+                break;
+            case 6:
+                curObj = image6;
+                break;
+            case 7:
+                curObj = image7;
+                break;
+            case 8:
+                curObj = image8;
+                break;
+            case 9:
+                curObj = image9;
+                break;
+            case 10:
+                curObj = image10;
+                break;
+            default:
+                Debug.Log("outside values");
+                break;             
+        }
+        Instantiate(curObj, transform.position, transform.rotation);
+        curImage = image1.name;
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<timer>().startTimmer();
+    }
+
+    public void nextImage()
+    {
+        imageCount++;
+        spawnImage();
+    }
+
+
+
+}
---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class timer : MonoBehaviour
+{
+    public float g_time;
+    public bool timmerOn;
+    private imageSwapnScript m_img;
+    // Use this for initialization
+    void Start()
+    {
+        timmerOn = false;
+        m_img = GameObject.FindGameObjectWithTag("GameController").GetComponent<imageSwapnScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timmerOn)
+        {
+            g_time += Time.deltaTime;
+        }
+        if (g_time > 5)
+        {
+            saveTimmer();
+            m_img.nextImage();
+            timmerOn = false;
+        }
+    }
+
+    public void startTimmer()
+    {
+        g_time = 0;
+        timmerOn = true;
+    }
+
+    public void saveTimmer()
+    {
+        timmerOn = false;
+        string temp = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+        StreamWriter sw = new StreamWriter(temp + "/trail1.txt", true);
+        if (g_time > 5)
+        {
+            sw.WriteLine(m_img.curImage + " Not Found");
+        }
+        else
+        {
+            sw.WriteLine(m_img.curImage + " " + g_time);
+        }
+
+        sw.Close();
+    }
+
+}
+
+
